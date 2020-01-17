83b9ee34308e0f4246703317a0515f69855ac987
Vít Listík <tivvit@seznam.cz>
2020-01-17 12:38:37 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 # x tools (installed by brew)
 # brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install coreutils golang python3 git gpg ansible terraform htop mc zsh hstr
+brew install coreutils golang python3 git gpg ansible terraform htop mc zsh hstr ntfs-3g
 
 # set default shell (zsh)
 chsh -s /bin/zsh
@@ -12,31 +12,19 @@ chsh -s /bin/zsh
 # copy ssh keys
 # dotfiles
 
-tunnelblick
-fuse
-ntfs
-docker
-copy clip
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # resh
 curl -fsSL https://raw.githubusercontent.com/curusarn/resh/master/scripts/rawinstall.sh | bash
 
 # widow apps
-brew cask install jetbrains-toolbox keybase activitywatch
+brew cask install jetbrains-toolbox keybase activitywatch google-chrome google-backup-and-sync tunnelblick docker copyclip fuse android-file-transfer firefox iterm2 steam visual-studio-code openscad calibre shotcut
 # install GoLand PyCharm PhpStorm CLion
 
-magnet
-android file transfer
-google backup and sync
-chrome
-firefox
-iterm2
-lime chat
-sli3r
-steam
-magnet
-
+# magnet
+# blackbaze 
+# lime chat
+# sli3r
 
 ###
 # Seznam.cz
@@ -48,13 +36,8 @@ magnet
 security import ~/szn/certs/tt-elk-tool1.ko-key.p12
 # sudo security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" server-cert.pem
 
-???
-vscode
-airdroid
-obs
-shotcut
-openscad
-blender
-calibre
-
-clean my mac x ?
+# ???
+# airdroid
+# obs
+# blender
+# clean my mac x
